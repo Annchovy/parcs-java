63d7b5ba8514fdd4445b0457f6071b08ed32f88d
teros0 <best.guess.oni@gmail.com>
2020-04-28 21:30:19 +0300

    add channels

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import parcs.*;
@@ -13,21 +14,48 @@ public class Main{
         AMInfo info = new AMInfo(curtask, null);
 //        Graph graph = new Graph("graph");
         int len = graph.names.size();
+        List<String> array_names = new ArrayList<>();
         for (int i = 0; i < 4; i ++){
             List subnames = graph.names.subList(i*len/4, i*len/4 + len/4);
             String names = "";
             for (int j = 0; j < subnames.size(); j++){
                 names = names + " " + subnames.get(j);
             }
+            array_names.add(names);
             System.out.println(names);
-            point p = info.createPoint();
-            channel c = p.createChannel();
-            p.execute("DijkstraParcs");
-            c.write(graph);
-            c.write(names);
-            System.out.println("Waiting for result...");
-            System.out.println("Result: " + (String) c.readObject());
-            curtask.end();
         }
+        point p1 = info.createPoint();
+        channel c1 = p1.createChannel();
+        p1.execute("DijkstraParcs");
+        c1.write(graph);
+        c1.write(array_names.get(0));
+
+        point p2 = info.createPoint();
+        channel c2 = p2.createChannel();
+        p2.execute("DijkstraParcs");
+        c2.write(graph);
+        c2.write(array_names.get(1));
+
+        point p3 = info.createPoint();
+        channel c3 = p3.createChannel();
+        p3.execute("DijkstraParcs");
+        c3.write(graph);
+        c3.write(array_names.get(2));
+
+        point p4 = info.createPoint();
+        channel c4 = p4.createChannel();
+        p4.execute("DijkstraParcs");
+        c4.write(graph);
+        c4.write(array_names.get(3));
+
+        System.out.println("Waiting for result...");
+        System.out.println("Result: " + (String) c1.readObject());
+        System.out.println("Waiting for result...");
+        System.out.println("Result: " + (String) c2.readObject());
+        System.out.println("Waiting for result...");
+        System.out.println("Result: " + (String) c3.readObject());
+        System.out.println("Waiting for result...");
+        System.out.println("Result: " + (String) c4.readObject());
+        curtask.end();
     }
 }
