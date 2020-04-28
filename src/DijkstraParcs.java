77868dece1084010d8ad980d9af57c2660c36f6d
teros0 <best.guess.oni@gmail.com>
2020-04-28 21:09:49 +0300

    print

--- src/DijkstraParcs.java
+++ src/DijkstraParcs.java
@@ -11,8 +11,10 @@ public class DijkstraParcs implements AM {
         String[] splited = nodes.split(" ");
         for (String name : splited) {
             try {
+                System.out.println(name);
                 Graph new_graph = Dijkstra.calculateShortestPathFromSource(graph, graph.hashNodes.get(name));
                 String result = new_graph.printDistances(graph.hashNodes.get(name));
+                System.out.println(result);
                 info.parent.write(result);
             } catch (IOException ex) {
 
